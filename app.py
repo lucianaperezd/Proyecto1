85fa31a1fb12da3e3a12663f5b784817bb463cec
lucianaperezd <lucianaperezdu@gmail.com>
2023-08-12 12:27:15 -0500

    app version 0.01

---
+++ app.py
@@ -0,0 +1,24 @@
+import dash
+from dash import dcc
+from dash import html
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.H1("Mi Dashboard"),
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': 'Montreal'},
+            ],
+            'layout': {
+                'title': 'Ejemplo de Gráfico'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
